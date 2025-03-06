baf3c0ba4c973bdb43b3482212d4f4392f00ddac
chikpan <chikpanwong@csus.edu>
2025-03-05 21:43:37 -0800

    [CWDriver.java, CWWindowManager.java]: Updated CWWindowManager not perfect
    Updated CWWindowManager but there is some problem(no argument constructor, no usage setWinWidth and getWindowSize)

--- src/main/java/pkgCWWindowsManager/CWWindowManager.java
+++ src/main/java/pkgCWWindowsManager/CWWindowManager.java
@@ -1,10 +1,13 @@
 package pkgCWWindowsManager;
 
 
+import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
 
-import static org.lwjgl.glfw.GLFW.glfwSetFramebufferSizeCallback;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
 import static org.lwjgl.opengl.GL11C.glViewport;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class CWWindowManager {
 
@@ -16,36 +19,59 @@ public class CWWindowManager {
                 }
             };
 
-    private static CWWindowManager my_window = new CWWindowManager();
+    private static CWWindowManager my_window;
     private static long glfwWindow;
     private static int win_height;
     private static int win_width;
 
-    private CWWindowManager(){
-
+    private CWWindowManager(int width, int height) {
+        this.win_width = width;
+        this.win_height = height;
+        my_window = this;
+        initGlfwWindow();
     }
 
     public void updateContextToThis(){
-
+        glfwMakeContextCurrent(glfwWindow);
     }
 
-    public void  destroyGlfwWindow(){
-
+    public void destroyGlfwWindow(){
+        glfwDestroyWindow(glfwWindow);
     }
 
     public boolean isGlfwWindowClosed(){
-
-    }
-
-    public static CWWindowManager get(int OrgX, int OrgY){
-
+        return glfwWindowShouldClose(glfwWindow);
     }
 
     public static CWWindowManager get(){
-
+        if (my_window == null) {
+            // Default size from asm 1
+            my_window = new CWWindowManager(1800, 1200);
+        }
+        return my_window;
     }
 
-    protected static void setWinWidth(int height, int width){
+    public static CWWindowManager get(int width, int height){
+        if (my_window == null) {
+            my_window = new CWWindowManager(width, height);
+        }
+        return my_window;
+    }
+
+    public static void setWindowPosition(int orgX, int orgY) {
+        if (glfwWindow > 0) {
+            glfwSetWindowPos(glfwWindow, orgX, orgY);
+        }  //  if (glfwWindow > 0)
+    }  //  public void setWindowPosition(...)
+
+    public static CWWindowManager get(int width, int height, int orgX, int orgY) {
+        get(width, height);
+        setWindowPosition(orgX, orgY);
+        return my_window;
+    }  //  public SlWindowManager get(...)
+
+
+    protected static void setWinWidth(int width, int height){
         win_height = height;
         win_width = width;
     }
@@ -55,15 +81,38 @@ public class CWWindowManager {
     }  //  public void enableResizeWindowCallback(...)
 
     public void swapBuffers(){
-
+        glfwSwapBuffers(glfwWindow);
     }
 
-    public void initGlfwWindow(){
+    // from asm 1
+    private static void initGlfwWindow(){
+
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_SAMPLES, 8);
+
+        glfwWindow = glfwCreateWindow(win_width, win_height, "CSC 133", NULL, NULL);
+
+        if (glfwWindow == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        my_window.updateContextToThis();
+        my_window.enableResizeWindowCallback();
+
+        // Default position 30, 90 from asm 1 driver
+        setWindowPosition(30,90);
+        int VSYNC_INTERVAL = 1;
+        glfwSwapInterval(VSYNC_INTERVAL);
+        glfwShowWindow(glfwWindow);
 
     }
 
     public int[] getWindowSize(){
-        return
+        return new int[]{win_width,win_height};
     }
 
 
